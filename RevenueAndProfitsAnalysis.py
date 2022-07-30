9a129ff36b7a3f475c7874673b65529aa359e1c1
ndlovucyprian <103043227+ndlovucyprian@users.noreply.github.com>
2022-07-30 10:59:37 +0200

    Update RevenueAndProfitsAnalysis.py

--- RevenueAndProfitsAnalysis.py
+++ RevenueAndProfitsAnalysis.py
@@ -7,8 +7,7 @@ Created on Sat Jul 30 09:35:34 2022
 import requests
 import matplotlib.pyplot as plt
 
-api_key='24d1f765af7e6651138b60ed7c26e38c'
-
+api_key=open('fmp_api_key','r').read()
 company="AAPL"
 years=10
 
@@ -20,7 +19,7 @@ title_revenue_and_profits= company + ' Revenue and Profits'
 income_statement=requests.get(f"https://financialmodellingprep.com/api/v3/income-statement/{company}?limit={years}&apikey={api_key}")
 
 income_statement=income_statement.json()
-
+print(income_statement)
 revenues=list(reversed([income_statement[i]['revenue'] for i in range(len(income_statement))]))
 profits=list(reversed([income_statement[i]['grossProfit'] for i in range(len(income_statement))]))
 
@@ -29,5 +28,4 @@ plt.plot(profits, label='Profit')
 plt.title(title_revenue_and_profits)
 plt.legend(loc='upper left')
 plt.show()
-
 
